320a089c9f6bc4205642ee5b9117826e3a9d9419
Sksvarson <jakub.szkwareq@gmail.com>
2025-04-25 13:42:15 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
 self.addEventListener("install", e => {
-    e.waitUntil(
-      caches.open("static").then(cache => {
-        return cache.addAll(["index.html", "style.css", "main.js"]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", e => {
-    e.respondWith(
-      caches.match(e.request).then(response => {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "script.js",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
