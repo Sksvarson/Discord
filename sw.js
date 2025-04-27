950f6a283acfc93bdc2fcdc5484f36608c04c806
Sksvarson <jakub.szkwareq@gmail.com>
2025-04-27 12:48:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-v6"; // <<--- zmieniaj numer przy każdej nowej wersji
+const CACHE_NAME = "static-v7"; // <<--- zmieniaj numer przy każdej nowej wersji
 const FILES_TO_CACHE = [
   "index.html",
   "style.css",
